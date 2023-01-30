5ec74172b2dab89daa942382a571255ccb2835e3
romaha <roma_work57@mail.ru>
2023-01-30 22:51:48 +0300

    add Docker-compose.yml & Dockerfile

--- store/store/settings.py
+++ store/store/settings.py
@@ -50,7 +50,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 DOMAIN_NAME = env('DOMAIN_NAME')
 
